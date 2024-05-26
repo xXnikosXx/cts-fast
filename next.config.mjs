592e4a86efbff362e80883ac03b3c2b34764fce7
Nikolaos Motos <nmotos@ctsolutions.gr>
2024-05-26 05:10:52 +0300

    Fixing error on build: EAGAIN no -11 - updating experimental cpu feature

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+require("dotenv").config();
+
+module.exports = {
+  distDir: 'build',
+  experimental: {
+    // This is experimental but can
+    // be enabled to allow parallel threads
+    // with nextjs automatic static generation
+    workerThreads: false,
+    cpus: 4
+  }
+};
