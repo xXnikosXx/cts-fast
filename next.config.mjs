61276fcbb89d2a29ab4b95164dec8375c7623f37
Nikolaos Motos <nmotos@ctsolutions.gr>
2024-05-26 19:29:02 +0300

    trying to deploy on vercel - removed changes in next config.

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,21 @@
-// @ts-check
+// // @ts-check
 
-export default (phase, { defaultConfig }) => {
-  /**
-   * @type {import('next').NextConfig}
-   */
-  const nextConfig = {
-    /* config options here */
-    distDir: 'build',
-    staticPageGenerationTimeout: 2000,
-  experimental: {
-    // This is experimental but can
-    // be enabled to allow parallel threads
-    // with nextjs automatic static generation
-    workerThreads: false,
-    cpus: 4
-  }
-  }
-  return nextConfig
-}
+// export default (phase, { defaultConfig }) => {
+//   /**
+//    * @type {import('next').NextConfig}
+//    */
+//   const nextConfig = {
+//     /* config options here */
+//     output: 'export',
+//     distDir: 'build',
+//     staticPageGenerationTimeout: 2000,
+//   experimental: {
+//     // This is experimental but can
+//     // be enabled to allow parallel threads
+//     // with nextjs automatic static generation
+//     workerThreads: false,
+//     cpus: 4
+//   }
+//   }
+//   return nextConfig
+// }
