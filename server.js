f923b9b8caabb75715a590649b60359cb5346176
Nikolaos Motos <nmotos@ctsolutions.gr>
2024-05-27 04:19:12 +0300

    .

--- server.js
+++ server.js
@@ -34,6 +34,16 @@ app.prepare().then(() => {
       console.log(email)
     try {
 
+        const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: parseInt(process.env.SMTP_PORT, 10),
+    secure: process.env.SMTP_PORT === '465',
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+  });
+
       // Send an email to the specified address (you)
     await transporter.sendMail({
       from: `"Newsletter signup (ctsolutions.gr)" <${process.env.SMTP_USER}>`,
